a7251355b094d32c784ea58095c114f67ebde994
bartek <mail@bartekpawlowski.pl>
2017-02-12 15:25:45 +0100

    Changes in display total price, localized strings in MainActivity.java

--- app/src/main/java/pl/bartekpawlowski/justjava/MainActivity.java
+++ app/src/main/java/pl/bartekpawlowski/justjava/MainActivity.java
@@ -7,39 +7,41 @@ import android.widget.TextView;
 
 import java.text.NumberFormat;
 
+import static android.R.id.message;
+
 /**
  * This app displays an order form to order coffee.
  */
 public class MainActivity extends AppCompatActivity {
 
-    private static int numberOfCoffees = 0;
+    private int numberOfCoffees = 0;
+    private int priceOfCoffee = 5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        display(this.numberOfCoffees);
+        displayPrice(this.numberOfCoffees);
     }
 
     /**
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        display(this.numberOfCoffees);
         displayPrice(this.numberOfCoffees);
-    }
+        }
 
     public void decrementQuantity(View view) {
         this.numberOfCoffees--;
         TextView quantityTextView = (TextView) findViewById(R.id.quantity);
         quantityTextView.setText("" + this.numberOfCoffees);
-        displayPrice(this.numberOfCoffees);
     }
 
     public void incrementQuantity(View view) {
         this.numberOfCoffees++;
         TextView quantityTextView = (TextView) findViewById(R.id.quantity);
         quantityTextView.setText("" + this.numberOfCoffees);
-        displayPrice(this.numberOfCoffees);
     }
 
     /**
@@ -54,8 +56,13 @@ public class MainActivity extends AppCompatActivity {
      * This method displays the given price on the screen.
      */
     private void displayPrice(int price) {
-        price *= 10;
+        String coffeEndWord = getString(R.string.coffee_single);
+        price *= this.priceOfCoffee;
+
+        if(this.numberOfCoffees > 1) {
+            coffeEndWord = getString(R.string.coffree_plural);
+        }
         TextView priceTextView = (TextView) findViewById(R.id.price);
-        priceTextView.setText(NumberFormat.getCurrencyInstance().format(price));
+        priceTextView.setText(getString(R.string.total) + ": " + NumberFormat.getCurrencyInstance().format(price) + " " + getString(R.string.for_sentence) + " " + this.numberOfCoffees + " " + coffeEndWord);
     }
 }
